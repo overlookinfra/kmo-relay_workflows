b1b1bb63ecb8de62d156b7a116006d5a9a7b0bc5
Alex Williamson <alex.williamson@new-host-3.home>
2021-11-29 16:38:25 -0500

    adding conditional checks

--- get_learndot_classes.sh
+++ get_learndot_classes.sh
@@ -2,6 +2,14 @@ echo "Running github curl script"
 
 TOKEN="TrainingRocket-Authorization: $ACCESS_TOKEN"
 
+
+curl -H 'content-type: application/json' \
+-H 'Accept: application/json' \
+-H "$TOKEN" \
+-d "{ \"startTime\" : [\"$START_DATE\", \"$END_DATE\"] }" \
+https://learn.puppet.com/api/rest/v2/manage/course_event/search \
+| jq -r '.results[] | "\(._displayName_) | \(.id)"'
+
 RESPONSE=$(curl -H 'content-type: application/json' \
 -H 'Accept: application/json' \
 -H "$TOKEN" \
@@ -10,6 +18,16 @@ https://learn.puppet.com/api/rest/v2/manage/course_event/search \
 | jq -r '.results[] | "\(._displayName_) | \(.id)"')
 
 
-echo "response data was: $RESPONSE"
-echo "setting response output value with ni"
-ni output set -k response -v "$RESPONSE"
+
+if [ $RESPONSE ]
+    echo "response data was: $RESPONSE"
+    echo "setting response output value with ni"
+    ni output set -k response -v "$RESPONSE"
+    echo "setting conditional flag to TRUE"
+    ni output set -k results_flag -v "TRUE"
+else
+    echo "No results in response: $RESPONSE"
+fi
+
+
+
