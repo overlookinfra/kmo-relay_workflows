ead210ceb5b0450df84824add8a40af6e25dd40a
Alex Williamson <alex.williamson@new-host-3.home>
2021-11-29 14:34:06 -0500

    adding response value set with ni

--- get_learndot_classes.sh
+++ get_learndot_classes.sh
@@ -1,12 +1,14 @@
 echo "Running github curl script"
 
-echo "Running ni get test"
-
 TOKEN="TrainingRocket-Authorization: $ACCESS_TOKEN"
 
-curl -H 'content-type: application/json' \
+RESPONSE=$(curl -H 'content-type: application/json' \
 -H 'Accept: application/json' \
 -H "$TOKEN" \
 -d'{ "startTime" : ["2021-11-16 17:19:11", "2021-11-20 17:19:11"] }' \
 https://learn.puppet.com/api/rest/v2/manage/course_event/search \
-| jq
+| jq)
+
+
+echo "setting response output value with ni"
+ni output set -k response -v $RESPONSE
