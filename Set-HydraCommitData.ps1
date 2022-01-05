c66a2272800fee05c63dc7cc4cb2740065bb9b89
Alex Williamson <alex.williamson@new-host-5.home>
2022-01-05 15:50:35 -0500

    adding event object list debugging

--- Set-HydraCommitData.ps1
+++ Set-HydraCommitData.ps1
@@ -10,3 +10,10 @@ foreach ($event in $eventListString) {
     Write-Output "Checking for event item:"
     Write-Output $event
 }
+
+$eventListObjects = $eventListString | ConvertFrom-Json
+
+foreach ($event in $eventListObjects) {
+    Write-Output "Event object list:"
+    Write-Output $event
+}
