d52b33efc7e9735b4c5693bd1423993a96200fd5
Alex Williamson <alex.williamson@new-host-5.home>
2022-01-05 09:46:03 -0500

    adding token type debug output

--- Get-DoceboToken.ps1
+++ Get-DoceboToken.ps1
@@ -17,5 +17,6 @@ Write-Output "Using Docebo URI: $($uri)"
 $getTokenResponse = Invoke-RestMethod -Uri $uri -Method Post -Form $form
 
 Write-Output "Setting access token output value"
+Write-Output "Retrived token type: $($getTokenResponse.token_type)"
 
 Relay-Interface output set -k DoceboToken -v $($getTokenResponse.access_token)
