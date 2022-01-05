c33d70fbce5e289b310876b35fa9b332f0b97f35
Alex Williamson <alex.williamson@new-host-5.home>
2022-01-05 10:30:31 -0500

    type debugging for export

--- Set-HydraCommitData.ps1
+++ Set-HydraCommitData.ps1
@@ -3,3 +3,5 @@ Write-Output "Fetching event list export"
 $eventList = (Relay-Interface get -p "${outputs.'Export-Docebo-Events'.'EventListExport'}")
 
 Write-Output $eventList 
+
+Write-Output "eventList is type $eventList.GetType()"
