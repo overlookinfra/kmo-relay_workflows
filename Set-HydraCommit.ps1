12a7b936d7c7bc99091e91fe8861a96a8c42f814
Alex Williamson <alex.williamson@new-host-5.home>
2022-01-06 10:01:26 -0500

    hunting string escape problems

--- Set-HydraCommit.ps1
+++ Set-HydraCommit.ps1
@@ -1,7 +1,7 @@
 # Get some mock data from Docebo
 Write-Output "Getting data from Docebo:"
 $uri = "https://training.puppet.com/course/v1/courses"
-Write-Output "Using uri: $$uri"
+Write-Output "Using uri: $($uri)"
 Write-Output "Setting up auth header"
 $headers = @{
     Authorization = "Bearer $($env:DoceboToken)"
@@ -9,10 +9,10 @@ $headers = @{
 
 $response = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
 
-Write-Output "Returned unfiltered data $response.data.items"
+Write-Output "Returned unfiltered data $($response.data.items)"
 
 $filteredList = $response.data.items | Where-Object {$_.last_update -lt "2021-11-15 00:00:00"}
 
 Write-Output "Filtered list:"
-Write-Output "$filteredList"
+Write-Output "$($filteredList)"
 
