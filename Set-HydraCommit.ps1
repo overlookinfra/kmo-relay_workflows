c1526377f4d867299b4b07196453e3a21f93e1ad
Alex Williamson <alex.williamson@new-host-5.home>
2022-01-06 10:19:53 -0500

    consolidating fixes

--- Set-HydraCommit.ps1
+++ Set-HydraCommit.ps1
@@ -3,14 +3,15 @@ Write-Output "Getting data from Docebo:"
 $uri = "https://training.puppet.com/course/v1/courses"
 Write-Output "Using uri: $($uri)"
 Write-Output "Setting up auth header"
-$headers = @(Authorization = "Bearer $($env:DoceboToken)")
+$headers = @{Authorization = "Bearer $($env:DoceboToken)"}
     
 $response = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
 
-Write-Output "Returned unfiltered data $($response.data.items)"
+Write-Output "Returned unfiltered data: "
+Write-Output $response.data.items
 
-$filteredList = $response.data.items | Where-Object ($_.last_update -lt "2021-11-15 00:00:00")
+$filteredList = $response.data.items | Where-Object {$_.last_update -lt "2021-11-15 00:00:00"}
 
-Write-Output "Filtered list:"
-Write-Output "$($filteredList)"
+Write-Output "Filtered list: "
+Write-Output $filteredList
 
